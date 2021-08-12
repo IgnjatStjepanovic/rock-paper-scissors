9603afe5fbf88ebfd109fbd18e7354745c020073
Ignjat Stjepanovic <53408313+Phalanxz@users.noreply.github.com>
2021-08-12 19:24:09 +0200

    Implemented most of the game logic. Usability details still to be done.

--- app.js
+++ app.js
@@ -1,12 +1,98 @@
-let pressedButtonRock = document.getElementById('playerrock')
-let pressedButtonPaper = document.getElementById('playerpaper')
-let pressedButtonScissor = document.getElementById('playerscissor')
+const buttonRock = document.getElementById("playerrock");
+const buttonPaper = document.getElementById("playerpaper");
+const buttonScissor = document.getElementById("playerscissor");
+const scorePlayer = document.getElementById("scoreplayer");
+const scoreCpu = document.getElementById("scorecpu");
+const buttonReset = document.getElementById("resetbutton");
+const roundResult = document.getElementById("roundresult");
 
-function getPressedButton (event) {
-    console.log(event.target.id)
+buttonRock.addEventListener("click", function () {
+  game("rock");
+});
+
+buttonPaper.addEventListener("click", function () {
+  game("paper");
+});
+
+buttonScissor.addEventListener("click", function () {
+  game("scissor");
+});
+
+buttonReset.addEventListener("click", function () {
+  location.reload();
+});
+
+let checker = 0;
+
+function game(userChoice) {
+  const computerChoice = getComputerchoice();
+
+  if (userChoice == "rock" && computerChoice == "rock") {
+    document.getElementById("cpusymbol").src = "rock.png";
+    checker = 0;
+  }
+
+  if (userChoice == "paper" && computerChoice == "paper") {
+    document.getElementById("cpusymbol").src = "paper.png";
+    checker = 0;
+  }
+
+  if (userChoice == "scissor" && computerChoice == "scissor") {
+    document.getElementById("cpusymbol").src = "scissor.png";
+    checker = 0;
+  }
+
+  if (userChoice == "rock" && computerChoice == "scissor") {
+    document.getElementById("cpusymbol").src = "scissor.png";
+    checker = 1;
+  }
+
+  if (userChoice == "rock" && computerChoice == "paper") {
+    document.getElementById("cpusymbol").src = "paper.png";
+    checker = 2;
+  }
+
+  if (userChoice == "paper" && computerChoice == "rock") {
+    document.getElementById("cpusymbol").src = "rock.png";
+    checker = 1;
+  }
+
+  if (userChoice == "paper" && computerChoice == "scissor") {
+    document.getElementById("cpusymbol").src = "scissor.png";
+    checker = 2;
+  }
+
+  if (userChoice == "scissor" && computerChoice == "paper") {
+    document.getElementById("cpusymbol").src = "paper.png";
+    checker = 1;
+  }
+
+  if (userChoice == "scissor" && computerChoice == "rock") {
+    document.getElementById("cpusymbol").src = "rock.png";
+    checker = 2;
+  }
+
+  if (checker == 0) {
+    console.log("DRAW");
+  }
+
+  if (checker == 1) {
+    let scoreplayer = document.getElementById("scoreplayer");
+    let current = parseInt(scoreplayer.innerHTML);
+    scoreplayer.innerHTML = current + 1;
+    console.log("I WON");    
+  }
+
+  if (checker == 2) {
+    let scorecpu = document.getElementById("scorecpu");
+    let current = parseInt(scorecpu.innerHTML);
+    scorecpu.innerHTML = current + 1;
+    console.log("I LOST");
+  }
 }
 
-pressedButtonRock.onclick = getPressedButton;
-pressedButtonPaper.onclick = getPressedButton;
-pressedButtonScissor.onclick = getPressedButton;
-
+function getComputerchoice(max) {
+  const choices = ["rock", "paper", "scissor"];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
+}
