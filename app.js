30cb37d9149ca97d4f99993f7921f35c1b17883d
Ignjat Stjepanovic <53408313+Phalanxz@users.noreply.github.com>
2021-08-12 23:52:44 +0200

    Last details of game logic and some styles implemented.

--- app.js
+++ app.js
@@ -29,65 +29,114 @@ function game(userChoice) {
 
   if (userChoice == "rock" && computerChoice == "rock") {
     document.getElementById("cpusymbol").src = "rock.png";
+    document.getElementById("playerchoice").innerHTML = "Rock";
+    document.getElementById("cpuchoice").innerHTML = "Rock";
     checker = 0;
   }
 
   if (userChoice == "paper" && computerChoice == "paper") {
     document.getElementById("cpusymbol").src = "paper.png";
+    document.getElementById("playerchoice").innerHTML = "Paper";
+    document.getElementById("cpuchoice").innerHTML = "Paper";
     checker = 0;
   }
 
   if (userChoice == "scissor" && computerChoice == "scissor") {
     document.getElementById("cpusymbol").src = "scissor.png";
+    document.getElementById("playerchoice").innerHTML = "Scissor";
+    document.getElementById("cpuchoice").innerHTML = "Scissor";
     checker = 0;
   }
 
   if (userChoice == "rock" && computerChoice == "scissor") {
     document.getElementById("cpusymbol").src = "scissor.png";
+    document.getElementById("playerchoice").innerHTML = "Rock";
+    document.getElementById("cpuchoice").innerHTML = "Scissor";
     checker = 1;
   }
 
   if (userChoice == "rock" && computerChoice == "paper") {
     document.getElementById("cpusymbol").src = "paper.png";
+    document.getElementById("playerchoice").innerHTML = "Rock";
+    document.getElementById("cpuchoice").innerHTML = "Paper";
     checker = 2;
   }
 
   if (userChoice == "paper" && computerChoice == "rock") {
     document.getElementById("cpusymbol").src = "rock.png";
+    document.getElementById("playerchoice").innerHTML = "Paper";
+    document.getElementById("cpuchoice").innerHTML = "Rock";
     checker = 1;
   }
 
   if (userChoice == "paper" && computerChoice == "scissor") {
     document.getElementById("cpusymbol").src = "scissor.png";
+    document.getElementById("playerchoice").innerHTML = "Paper";
+    document.getElementById("cpuchoice").innerHTML = "Scissor";
     checker = 2;
   }
 
   if (userChoice == "scissor" && computerChoice == "paper") {
     document.getElementById("cpusymbol").src = "paper.png";
+    document.getElementById("playerchoice").innerHTML = "Scissor";
+    document.getElementById("cpuchoice").innerHTML = "Paper";
     checker = 1;
   }
 
   if (userChoice == "scissor" && computerChoice == "rock") {
     document.getElementById("cpusymbol").src = "rock.png";
+    document.getElementById("playerchoice").innerHTML = "Scissor";
+    document.getElementById("cpuchoice").innerHTML = "Rock";
+
     checker = 2;
   }
 
-  if (checker == 0) {
-    console.log("DRAW");
+  if (checker == 0) {    
+    let draws = document.getElementById("drawsnumber");
+    let drawsint = parseInt(draws.innerHTML);
+    draws.innerHTML = drawsint + 1;
+    document.getElementById("roundresult").innerHTML = "Draw!";
   }
 
   if (checker == 1) {
     let scoreplayer = document.getElementById("scoreplayer");
     let current = parseInt(scoreplayer.innerHTML);
     scoreplayer.innerHTML = current + 1;
-    console.log("I WON");    
+    document.getElementById("roundresult").innerHTML = "You win!";
   }
 
   if (checker == 2) {
     let scorecpu = document.getElementById("scorecpu");
     let current = parseInt(scorecpu.innerHTML);
     scorecpu.innerHTML = current + 1;
-    console.log("I LOST");
+    document.getElementById("roundresult").innerHTML = "CPU wins!";
+  }
+
+  let currentRound = document.getElementById("roundnumber");
+  let currentRoundParsed = parseInt(currentRound.innerHTML);
+  currentRound.innerHTML = currentRoundParsed + 1;
+
+  if (currentRoundParsed > 14) {
+    let scorecpu = document.getElementById("scorecpu");
+    let scoreplayer = document.getElementById("scoreplayer");
+    let currentcpu = parseInt(scorecpu.innerHTML);
+    let currentplayer = parseInt(scoreplayer.innerHTML);
+
+    if (currentcpu > currentplayer) {
+      document.getElementById("roundtitle").innerHTML = "CPU is the winner! Press Reset to start again";
+    }
+
+    if (currentcpu < currentplayer) {
+      document.getElementById("roundtitle").innerHTML = "You are the winner! Press Reset to start again";
+    }
+
+    if (currentcpu == currentplayer) {
+      document.getElementById("roundtitle").innerHTML = "It's a draw! Press Reset to start again";
+    }
+
+
+
+
   }
 }
 
